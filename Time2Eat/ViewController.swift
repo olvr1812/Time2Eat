bb04290e493bec247d67e1823603f515892ff73d
Oliver <64903691+olvr1812@users.noreply.github.com>
2023-07-08 17:00:39 +0300

    Initial Commit

---
+++ Time2Eat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Time2Eat
+//
+//  Created by Оливер Салихов on 08.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
